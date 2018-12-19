e29f0bcc1f04c7704acd477fc25dfbc64ba3bb79
Gastonm1 <gaston.marques@gmail.com>
2018-12-18 21:59:40 -0600

    style crystals & random number for crystals

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,20 +1,34 @@
 // =================PSEUDO CODE===================================================================================
-// Start with 4 crystals
-// Every crystal needs to be assigned a random number between 1-12
+// Start with 4 crystals DONE
+// Every crystal needs to be assigned a random number between 1-12 DONE
 // A new number needs to be generated to each crystal every time user Wins or Loses
 // When any crystal is clicked, its value should be adding to the previous number until it matches the total.
-// if it is greater than RandNum, then we increment the loss counter; then reset
+// if it is greater than total, then we increment the loss counter; then reset
 // If it is equal, then add one win to the win counter, then reset
 
 //==================GLOBAL VARIABLES===============================================================================
-var randNum;
-var lost;
-var win;
+var total;
+var losses = 0;
+var wins = 0;
 //=================================================================================================================
 
-for (var i=0; i<4; i++) {
-    var crystal = $("<div>");
+// Initialization
 
-    $(".crystals").append(crystal);
-    console.log("Hello World")
+function init() {
+  var random1 = Math.floor(Math.random() * 12);
+  var random2 = Math.floor(Math.random() * 12);
+  var random3 = Math.floor(Math.random() * 12);
+  var random4 = Math.floor(Math.random() * 12);
+
+  $("#bluegem").attr("data-random-number", random1);
+
+  $("#greengem").attr("data-random-number", random2);
+
+  $("#purplegem").attr("data-random-number", random3);
+
+  $("#redgem").attr("data-random-number", random4);
 }
+
+init();
+
+$(".bluecrystal").click()
